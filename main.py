f210b684848b9c47e0168826d42bd895c27490c1
pnhmd <afonsocpimenta04@gmail.com>
2025-05-22 22:04:53 +0100

    chore: add docstrings and type hinting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 from mcp.server.fastmcp import FastMCP
 from server import run_server
 from utils import create_dynamic_function, load_provider_configuration
+from utils import ProviderInformation
 
 mcp = FastMCP("Rest API Proxy Server", "1.0.0")
 
@@ -12,7 +13,7 @@ subdirectories = [f.path for f in os.scandir("specifications") if f.is_dir()]
 if not subdirectories:
     raise Exception("No provider directories found in 'specifications' directory.")
 
-provider_info_dict = dict()
+provider_info_dict: dict[str, ProviderInformation] = {}
 # Read configuration from each provider
 for subdir in subdirectories:
     provider_name = os.path.basename(subdir)
@@ -30,9 +31,9 @@ if not provider_info_dict:
     
 # for each provided OpenAPI specification
 for provider_name, provider_info in provider_info_dict.items():
-    specification = provider_info['specification']
-    base_url = provider_info['base_url']
-    headers = provider_info['headers']
+    specification = provider_info.specification
+    base_url = provider_info.base_url
+    headers = provider_info.headers
     # for each endpoint
     for path in specification.paths:
         # for each operation (endpoint + method)
--- utils.py
+++ utils.py
@@ -2,17 +2,40 @@ import inspect
 import requests
 import os
 import json
-from typing import Optional
+from typing import NamedTuple, Optional
 from openapi_parser import parse
+from openapi_parser.specification import Specification, Operation, Path
 from openapi_parser.enumeration import ParameterLocation
 
+class ProviderInformation(NamedTuple):
+    """
+    A named tuple to hold provider information including OpenAPI specification, base URL, and headers.
+
+    Attributes:
+        specification (Specification): The OpenAPI specification object parsed from the specification file.
+        base_url (str): The base URL for the API parsed from config file.
+        headers (Optional[dict[str, str]]): Headers to be used in the request parsed from config file.
+    """
+    specification: Specification
+    base_url: str
+    headers: Optional[dict[str, str]]
+
 class FunctionParameter:
+    """
+    A class to represent a parameter to be passed to the dynamic function.
+
+    Attributes:
+        name (str): The name of the parameter.
+        required (bool): Indicates if the parameter is required.
+        description (str): A computed description of the parameter.
+        _in (ParameterLocation): The location of the parameter. Supports "query", "path" or "header".
+    """
     name: str
     required: bool
-    description: Optional[str] = None
-    _in: str
+    description: str
+    _in: ParameterLocation
     
-    def __init__(self, name: str, required: bool, _in: str, description: Optional[str] = None):
+    def __init__(self, name: str, required: bool, _in: ParameterLocation, description: str):
         self.name = name
         self.required = required
         self.description = name + " - " + description + f" ({'required' if required else 'optional'})"
@@ -25,14 +48,25 @@ class FunctionParameter:
             default=inspect.Parameter.empty if self.required else None
         )
 
-def create_dynamic_function(provider_name, operation, path, base_url, provider_headers):
+def create_dynamic_function(provider_name: str, operation: Operation, path: Path, base_url: str, provider_headers: dict[str, str]) -> callable:
     """
+    Create a dynamic function that makes an API request 
+    based on the information from the OpenAPI specification.
 
+    Args:
+        provider_name (str): The name of the provider.
+        operation (Operation): The OpenAPI operation object.
+        path (Path): The OpenAPI path object.
+        base_url (str): The base URL for the API.
+        provider_headers (dict): Headers to be used in the request.
+
+    Returns:
+        function: A dynamically created function that makes an API request.
     """
-    parameters = [] 
+    parameters: list[FunctionParameter] = []
     # Add parameters associated with operation
     for param in operation.parameters:
-        param_name = param.name.replace('-', '_')
+        param_name= param.name.replace('-', '_')
         new_parameter = FunctionParameter(
             # Remove invalid characters
             name=''.join(e for e in param_name if e.isalnum() or e == '_'),
@@ -97,7 +131,7 @@ def create_dynamic_function(provider_name, operation, path, base_url, provider_h
     dynamic_function.__name__ = clean_name
     
     # Set function docstring
-    doc = operation.description or f"Make a {operation.method.value} request to {path.url}"
+    doc = operation.description
     if parameters:
         doc += "\nArguments:"
         for param in parameters:
@@ -106,12 +140,25 @@ def create_dynamic_function(provider_name, operation, path, base_url, provider_h
 
     return dynamic_function
 
-def load_provider_configuration(subdir, provider_name):
+def load_provider_configuration(subdir: str, provider_name: str) -> ProviderInformation:
+    """
+    Loads the provider's OpenAPI specification, base URL, and headers from the configuration file.
+    The configuration file should be named 'config.json' and should contain the following keys:
+    - base_url: The base URL for the API.
+    - headers: A nested object of headers to be used in the request.
+
+    Args:
+        subdir (str): The path to the provider's directory.
+        provider_name (str): The name of the provider.
+    Returns:
+        ProviderInformation: A named tuple containing the OpenAPI specification, base URL, and headers.
+    """
     # Parse the OpenAPI specification
     specification = None
     for spec_filename in ['specification.json', 'specification.yaml', 'specification.yml']:
         spec_path = os.path.join(subdir, spec_filename)
         if os.path.exists(spec_path):
+            # Parse and validate the specification. Raises an error if invalid
             specification = parse(spec_path)
             print(f"Found and parsed {spec_filename} for provider {provider_name}.")
             break
@@ -119,10 +166,6 @@ def load_provider_configuration(subdir, provider_name):
     if not specification:
         raise Exception(f"No specification file found.")
 
-    provider_info = {
-        'specification': specification
-    }
-    
     # Load provider-specific configuration file
     config_path = os.path.join(subdir, 'config.json')
     with open(config_path, 'r') as config_file:
@@ -130,10 +173,14 @@ def load_provider_configuration(subdir, provider_name):
         # Override with configuration if available
         if config:
             if 'base_url' in config:
-                provider_info['base_url'] = config['base_url']
+                base_url = config['base_url']
             else:
                 raise Exception(f"base_url not found in config.json")
             if 'headers' in config:
-                provider_info['headers'] = config['headers']
+                headers = config['headers']
     
-    return provider_info
+    return ProviderInformation(
+        specification=specification,
+        base_url=base_url,
+        headers=headers
+    )
